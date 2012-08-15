b69c5e212cbdbc36a5fa24fdc308c4cc6db00fd7
Ben Taylor <ben.taylor.uk@gmail.com>
2012-08-15 17:31:43 +0100

    Added arrows between params.  Made code slightly more readable in FakeModels.  Removed StyleFake as not using (was added by me).

--- if-doc/FakeModels.fs
+++ if-doc/FakeModels.fs
@@ -3,75 +3,79 @@ open IntelliFactory.Documentation
 open System.Collections.Generic
 open System
 
+let join separator (args:_ seq) = String.Join(separator, args)
+
 let isDocableMember (m:CodeModel.IMember) = 
     m.Documentation.IsSome && m.Documentation.Value.IsUserDoc
 
-let mapMembers (members:CodeModel.IMember seq) =
-    members 
-    |> Seq.filter isDocableMember 
-    |> Seq.map (fun x -> 
-        let join separator (args:_ seq) = String.Join(separator, args)
+let summariseModuleMethod (mm:CodeModel.ModuleMethod) (doc:CodeModel.Documentation) = 
+    //TODO use the tokens pretty print instead of this hack.
+    mm.Parameters |> Seq.fold (fun (acc:string) param ->
+        let rec typeDisplay = function
+            | CodeModel.Type.ConcreteType(tr) -> tr.DisplayName
+            | CodeModel.Type.FunctionType(t1, t2) -> 
+                //if x.Documentation.Value.Defs.ContainsKey(param.Name) then
+                //   "Yo" |> ignore
+                "(" + typeDisplay t1 + " -> " + typeDisplay t2 + ")"//not convinced about my bracketing here.
+            | _ -> "TBD"
+        let text = typeDisplay param.Type
+        (*
+        | InferredType
+        | ConcreteType of TypeReference
+        | ArrayType of Type * int
+        | PointerType of Type
+        | ByReferenceType of Type
+        | GenericType of TypeReference * list<Type>
+        | ClassParameterType of GenericParameter
+        | MethodParameterType of GenericParameter
+        | TupleType of list<Type>
+        | FunctionType of Type * Type
+        | OptionalParameterType of Type
+        | UnitType
+        *)
+        let paramComments = 
+            let paramComments = doc.Parameters;
+            if paramComments.ContainsKey(param.Name) then
+                paramComments.[param.Name]
+            else "Comments for this parameter are missing."
+                
+        let xxx = sprintf "<span rel='popover' data-content='%s' data-original-title='%s'>%s</span>" paramComments text param.Name
+        let separator = if acc.Length = 0 then "" else " -> "
+        acc + separator + xxx + ":<span style='color:gray'>" + text + "</span> ")
+        ""
 
-        let summary = x.Documentation.Value.Summary.Replace("div", "span") 
-
-        let typesum = 
-            //TODO use the tokens pretty print instead of this hack.
-            match x with 
-            | :? CodeModel.ModuleMethod as mm -> 
-                mm.Parameters 
-                |> Seq.fold (fun acc param ->
-                    let rec typeDisplay = function
-                        | CodeModel.Type.ConcreteType(tr) -> tr.DisplayName
-                        | CodeModel.Type.FunctionType(t1, t2) -> 
-                            //if x.Documentation.Value.Defs.ContainsKey(param.Name) then
-                            //   "Yo" |> ignore
-                            "(" + typeDisplay t1 + " -> " + typeDisplay t2 + ")"//not convinced about my bracketing here.
-                        | _ -> "TBD"
-                    let text = typeDisplay param.Type
-                    (*
-                    | InferredType
-                    | ConcreteType of TypeReference
-                    | ArrayType of Type * int
-                    | PointerType of Type
-                    | ByReferenceType of Type
-                    | GenericType of TypeReference * list<Type>
-                    | ClassParameterType of GenericParameter
-                    | MethodParameterType of GenericParameter
-                    | TupleType of list<Type>
-                    | FunctionType of Type * Type
-                    | OptionalParameterType of Type
-                    | UnitType
-                    *)
-                    let paramComments = 
-                        let paramComments = x.Documentation.Value.Parameters;
-                        if paramComments.ContainsKey(param.Name) then
-                            paramComments.[param.Name]
-                        else "Comments for this parameter are missing."
-                            
-                    let xxx = sprintf "<span rel='popover' data-content='%s' data-original-title='%s'>%s</span>" paramComments text param.Name
-                    acc + xxx + ":<span style='color:gray'>" + text + "</span> "
-                ) ""
-            | _ -> ""
-
-        let ps = 
-            x.Documentation.Value.Parameters 
-            |> Seq.map (fun p -> p.Key + " " + p.Value) 
-            |> (join "<br/>")
-
-        [
-        "name", x.Name :> obj;
+let makeMemberMap (mem:CodeModel.IMember) =
+    let doc = mem.Documentation.Value
+    let summary = doc.Summary.Replace("div", "span") 
+    let typesum = 
+        match mem with 
+        | :? CodeModel.ModuleMethod as mm -> summariseModuleMethod mm doc
+        | _ -> ""
+    let tokenText = 
+        mem.Type.Tokens 
+        |> Seq.fold (fun acc token ->
+            match token with
+            | CodeModel.TypeToken.ReferenceToken(tr) -> acc + tr.DisplayName
+            | CodeModel.TypeToken.TextToken(text) -> acc + text)
+            ""
+    [   
+        "name", mem.Name :> obj;
         "summary", summary :> obj
-        "params", ps :> obj
         "typesum", typesum :> obj
-        ] |> Map.ofList
-        )
+        "tokentext", tokenText :> obj
+    ] 
+    |> Map.ofList
 
 let mapTypes (types:CodeModel.IType seq) =
     types 
     |> Seq.filter (fun t -> t.Methods |> Seq.filter isDocableMember |> Seq.length > 0)
     |> Seq.map (fun x -> 
         let name = x.Name.Replace("Helper", "")
-        ["name", name :> obj; "functions", (mapMembers x.Methods) :> obj] |> Map.ofList)
+        let memberMaps = 
+            x.Methods 
+            |> Seq.filter isDocableMember
+            |> Seq.map makeMemberMap
+        ["name", name :> obj; "functions", memberMaps :> obj] |> Map.ofList)
     
 let mapNs (namespaces:CodeModel.Namespace list) =
     namespaces 
@@ -89,15 +93,13 @@ let indexView (assSet:CodeModel.AssemblySet) =
             "name", ass.Name :> obj; 
             "namespaces", (mapNs ass.Namespaces) :> obj;
             ] 
-            |> Map.ofList
-        )
+            |> Map.ofList)
     [
-    "assemblies", asses :> obj;
-    "title", "FAKE - Build Automation for .NET" :> obj; 
+        "assemblies", asses :> obj;
+        "title", "FAKE - Build Automation for .NET" :> obj; 
     ] 
     |> Map.ofList
 
 let fakeViews (name:string) (assSet:CodeModel.AssemblySet) = 
     let viewMap = ["index", indexView] |> Map.ofList
     (viewMap.Item (name.ToLower())) assSet
-    
